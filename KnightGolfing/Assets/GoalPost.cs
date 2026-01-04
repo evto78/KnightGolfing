026a1ddb4f14824d9947e4034f27fbe9f1ec8609
Avi Nautiyal <avomanatee@gmail.com>
2026-01-04 18:58:48 -0500

    whats that goalpost doing there

--- KnightGolfing/Assets/GoalPost.cs
+++ KnightGolfing/Assets/GoalPost.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GoalPost : MonoBehaviour
 {
+    public float goalSlime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        goalSlime = 1.025f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.layer == 8)
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+
+            rb.velocity = rb.velocity / (goalSlime);
+        }
     }
 }
